a10426f8cadaab12a6b4b69207e06489c8731dd3
Vlad Goncharov <goncharov-vlad@ukr.net>
2019-12-25 00:58:14 +0200

    make proper regular expression

--- acronym.rb
+++ acronym.rb
@@ -1,14 +1,5 @@
 module Acronym
   def self.abbreviate(str)
-    #str.scan(/(\A\w|((?<=\s)|-))\w/).join.upcase
-    abbr = ""
-    str.split(/\s*-*/).each do |word|
-      abbr += " #{word}"
-    end
-    puts abbr
+    str.scan(/^\w|(?<=\s)\w|(?<=-)\w/).join.upcase
   end
 end
-
-include Acronym
-
-Acronym.abbreviate('Something - I made up from thin air')
