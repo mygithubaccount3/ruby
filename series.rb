381cdee923f9be7f123aa510dc2f55e63c39a0a4
Vlad Goncharov <goncharov-vlad@ukr.net>
2019-12-24 12:36:42 +0200

    initial commit

--- series.rb
+++ series.rb
@@ -6,8 +6,8 @@ class Series
   end
 
   def slices(num)
-    raise ArgumentError, "Too big argument" if num > @string_of_digits.length
-    arr_with_grouped_digits = @string_of_digits.chars.each_cons(num).to_a
+    raise ArgumentError, "Too big argument" if num > string_of_digits.length
+    arr_with_grouped_digits = string_of_digits.chars.each_cons(num).to_a
     arr_with_grouped_digits.each_with_index do |item, index|
       arr_with_grouped_digits[index] = item.join("")
     end
