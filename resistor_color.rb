e6b9a8af9c067afcb6bc4579ef6dd6a0835b5364
Vlad Goncharov <goncharov-vlad@ukr.net>
2019-12-27 12:17:29 +0200

    initial commit

--- resistor_color.rb
+++ resistor_color.rb
@@ -2,10 +2,6 @@ class ResistorColor
   COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 
   def self.color_code(color)
-    COLORS.each_with_index do |item, index|
-      if color == item
-        return index
-      end
-    end
+    COLORS.index(color)
   end
 end
